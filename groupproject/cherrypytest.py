2427729f110d86cbae0230fba7da25b52396fb1d
Brett Piatek <brett.piatek@g.austincc.edu>
2016-07-29 16:21:43 -0500

    Revert "Commit 7/29"
    This reverts commit 9569f1743c85fa8a5d908c658f95851bdb702c3f.

--- groupproject/cherrypytest.py
+++ groupproject/cherrypytest.py
@@ -1,22 +1,8 @@
 import cherrypy
 
-class Landing(object):		
+class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return """
-		<html>
-			<div id="slideshow">
-			<div>
-				<img src="//farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg">
-			</div>
-			<div>
-				<img src="//farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg">
-			</div>
-			<div>
-				Pretty cool eh? This slide is proof the content can be anything.
-			</div>
-			</div>
-		</html>"""
+        return "Hello World!"
 
-if __name__ == "__main__":
-    cherrypy.quickstart(Landing(), '/')
+cherrypy.quickstart(HelloWorld())
