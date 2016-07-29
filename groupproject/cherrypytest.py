9569f1743c85fa8a5d908c658f95851bdb702c3f
Brett Piatek <brett.piatek@g.austincc.edu>
2016-07-29 16:21:35 -0500

    Commit 7/29

--- groupproject/cherrypytest.py
+++ groupproject/cherrypytest.py
@@ -1,8 +1,22 @@
 import cherrypy
 
-class HelloWorld(object):
+class Landing(object):		
     @cherrypy.expose
     def index(self):
-        return "Hello World!"
+        return """
+		<html>
+			<div id="slideshow">
+			<div>
+				<img src="//farm6.static.flickr.com/5224/5658667829_2bb7d42a9c_m.jpg">
+			</div>
+			<div>
+				<img src="//farm6.static.flickr.com/5230/5638093881_a791e4f819_m.jpg">
+			</div>
+			<div>
+				Pretty cool eh? This slide is proof the content can be anything.
+			</div>
+			</div>
+		</html>"""
 
-cherrypy.quickstart(HelloWorld())
+if __name__ == "__main__":
+    cherrypy.quickstart(Landing(), '/')
