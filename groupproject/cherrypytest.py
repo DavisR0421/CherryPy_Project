0d9fe8bb3a8b3d0cb4007bc2476873eb5336fa3e
Brett Piatek <brett.piatek@g.austincc.edu>
2016-07-31 13:38:03 -0500

    Ready for image links
    ready for images

--- groupproject/cherrypytest.py
+++ groupproject/cherrypytest.py
@@ -1,8 +1,11 @@
 import cherrypy
 
-class HelloWorld(object):
+class Landing(object):		
     @cherrypy.expose
     def index(self):
-        return "Hello World!"
+        return """<html>
+					<img src="http://blogs-images.forbes.com/insertcoin/files/2015/07/zelda.png" style = "width:200px; height:300px;">
+				  </html>"""
 
-cherrypy.quickstart(HelloWorld())
+if __name__ == "__main__":
+    cherrypy.quickstart(Landing(), '/')
